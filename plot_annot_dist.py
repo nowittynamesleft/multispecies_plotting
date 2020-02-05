c43e7d867d7d437880014fd33248db9d80405ca2
Meet Barot <mmb557@nyu.edu>
2020-02-05 16:44:24 -0500

    git ignore and make plot_annot_dist.py work like it did before

--- plot_annot_dist.py
+++ plot_annot_dist.py
@@ -46,12 +46,14 @@ for ii in test_funcs:
 pickle.dump(common_goids, open("bacteria_" + go_type + "_train_goids.pckl", "wb")) # these are the goids for the thing
 """
 
+# This checks whether the single org annot list with the single org threshold has go terms that have at least 50 examples in the multi org list (i.e., more than 20 examples not in the single org annot list)
 
-
+single_pckl = str(sys.argv[1])
 multi_pckl = str(sys.argv[2])
 
 single_annot = pickle.load(open(single_pckl, 'rb'))
 multi_annot = pickle.load(open(multi_pckl, 'rb'))
+go_type = 'molecular_function'
 
 single_Y = single_annot['annot'][go_type].toarray()
 single_goids = single_annot['go_IDs'][go_type]
@@ -59,7 +61,7 @@ single_gonames = single_annot['go_names'][go_type]
 
 multi_Y = multi_annot['annot'][go_type].toarray()
 multi_prots = multi_annot['prot_IDs']
-idx = [ii for ii, prot in enumerate(multi_prots) if not prot.startswith("553174")]
+idx = [ii for ii, prot in enumerate(multi_prots) if not prot.startswith("553174")] # checks whether it's ecoli strain K12 substrain whatever (highest annotated bacteria)
 multi_prots = np.asarray(multi_prots)
 multi_prots = multi_prots[idx]
 multi_Y = multi_Y[idx]
@@ -69,15 +71,17 @@ test_funcs = np.where(np.logical_and(single_Y.sum(axis=0) > 30, single_Y.sum(axi
 
 common_goids = []
 common_gonames = []
+all_taxa = set()
 for ii in test_funcs:
     go_id = single_goids[ii]
     go_name = single_gonames[ii]
-    if go_id in multi_goids:
-        idx = multi_goids.index(go_id)
-        if sum(multi_Y[:, idx]) > 50:
+    if go_id in multi_goids: # if the test func for the single org annot thing is in the multigoid list
+        idx = multi_goids.index(go_id) 
+        if sum(multi_Y[:, idx]) > 50: # if the number of examples in the multi org annot are more than 50
             indx = np.where(multi_Y[:, idx] > 0)[0]
-            tmp_prots = multi_prots[indx]
-            tax = set([prot.split('.')[0] for prot in tmp_prots])
+            tmp_prots = multi_prots[indx] # 
+            tax = set([prot.split('.')[0] for prot in tmp_prots]) # taxa that are included in the multiple taxa annotations
+            all_taxa = all_taxa.union(tax)
             if len(tax) == 5:
                 common_goids.append(go_id)
                 common_gonames.append(go_name)
@@ -119,5 +123,5 @@ ax.set_xticklabels(common_gonames, fontsize=8, rotation=30, rotation_mode="ancho
 for tick in ax.xaxis.get_majorticklabels():
     tick.set_horizontalalignment("right")
 
-plt.savefig('annot_stats.png', bbox_inches='tight')
+plt.savefig('-'.join(all_taxa) + go_type + '_annot_stats_mult_single.png', bbox_inches='tight')
 # plt.show()
