39cb3a4334c24694a575eb59c222ca13465e61c2
Meet Barot <mmb557@nyu.edu>
2020-02-06 17:17:48 -0500

    Largest con comps with annot percs now

--- plot_largest_connected_comps.py
+++ plot_largest_connected_comps.py
@@ -6,10 +6,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 annot_file = sys.argv[1] # annot file for all organisms in this set
-network_files = sys.argv[2:] # individual network pickle files for all organisms in the set
+title_keyword = sys.argv[2]
+network_files = sys.argv[3:] # individual network pickle files for all organisms in the set
 
 net_ratios = []
 taxa = []
+num_nodes = []
 for network_file in network_files:
     taxon = network_file.split('_')[0]
     taxa.append(taxon)
@@ -21,6 +23,7 @@ for network_file in network_files:
     largest_con_comp = max(nx.connected_component_subgraphs(graph), key=len)
     nc = float(len(largest_con_comp))
     n = float(len(nx.nodes(graph)))
+    num_nodes.append(n)
     nc_over_n = nc/n
     net_ratios.append(nc_over_n)
     print(nc_over_n)
@@ -38,19 +41,27 @@ org_of_prots = np.array([prot.split('.')[0] for prot in prot_ids])
 species_inds = {}
 
 annot_ratios = []
-for taxon in taxa:
+for i, taxon in enumerate(taxa):
     species_inds[taxon] = np.where(org_of_prots == taxon)[0]
     num_annotated_prots = np.sum(all_branch_annots[species_inds[taxon]].any(1))
-    annot_ratios.append(float(num_annotated_prots)/float(len(species_inds[taxon])))
+    annot_ratios.append(float(num_annotated_prots)/float(num_nodes[i]))
 
 fig, ax = plt.subplots()
 x_positions = np.arange(len(taxa))
-width = 0.5
-rects_1 = ax.bar(x_positions-0.25, net_ratios, width, label='N_c/N')
-rects_2 = ax.bar(x_positions+0.25, annot_ratios, width, label='Proportion annotated')
+width = 0.3
+#rects_1 = ax.bar(x_positions-0.25, net_ratios, width, label='N_c/N')
+#rects_2 = ax.bar(x_positions+0.25, annot_ratios, width, label='Proportion annotated')
+
+sums = np.array(net_ratios) + np.array(annot_ratios)
+sort_inds = np.argsort(sums)
+sorted_taxa = np.array(taxa)[sort_inds]
+sorted_net_ratios = np.array(net_ratios)[sort_inds]
+sorted_annot_ratios = np.array(annot_ratios)[sort_inds]
+rects_1 = ax.bar(x_positions-0.15, sorted_net_ratios, width, label='N_c/N')
+rects_2 = ax.bar(x_positions+0.15, sorted_annot_ratios, width, label='Proportion annotated')
 
 ax.set_ylabel('N_c/N')
-ax.set_title('Ratio of largest connected components to total size of graph')
+ax.set_title('Ratio of largest connected components to total size of graph - ' + title_keyword)
 ax.set_xticks(x_positions)
 ax.set_xticklabels(taxa)
 
@@ -60,7 +71,7 @@ def autolabel(rects):
     """Attach a text label above each bar in *rects*, displaying its height."""
     for rect in rects:
         height = rect.get_height()
-        ax.annotate('{}'.format(height),
+        ax.annotate('{0:.2f}'.format(height),
                     xy=(rect.get_x() + rect.get_width() / 2, height),
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
