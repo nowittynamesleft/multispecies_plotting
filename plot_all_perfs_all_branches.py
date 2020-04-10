1e17c65eeb199e4c6919bb1ceafc163fa7046a7f
Meet Barot <mmb557@nyu.edu>
2020-04-09 20:38:41 -0400

    forgot to commit for a while

--- plot_all_perfs_all_branches.py
+++ plot_all_perfs_all_branches.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.use('TkAgg')
+#matplotlib.use('TkAgg')
 import sys
 import numpy as np
 from scipy.stats import sem
@@ -70,7 +70,7 @@ def evaluate_performance(y_test, y_score, y_pred):
     return pr_macro, pr_micro, acc, F1
 
 
-def load_perfs(fnames):
+def load_perfs(fnames, loso=False):
     # fnames: filenames of performances for different alphas (0.0 to 1.0 in increments of 0.1)
     macros = []
     macro_std_errs = []
@@ -87,18 +87,18 @@ def load_perfs(fnames):
             perfs = pickle.load(open(fname, "rb"))
             trial_macros = np.nanmean(perfs, axis=0)
         '''
-        if '_scores.pckl' in fname: # now assuming BLAST pred files
-            pred_file = pickle.load(open(fname, 'rb'))
-            num_trials = len(pred_file['Y_hat_test'])
-            print('BLAST baseline')
-            trial_macros = []
-            trial_micros = []
-            trial_accs = []
-            trial_f1s = []
-            for trial in range(0, num_trials):
+        if loso: # leave one species out; only one trial
+            if '_scores.pckl' in fname: # now assuming BLAST pred files
+                pred_file = pickle.load(open(fname, 'rb'))
+                num_trials = len(pred_file['Y_hat_test'])
+                print('BLAST baseline')
+                trial_macros = []
+                trial_micros = []
+                trial_accs = []
+                trial_f1s = []
                 print('Number of go ids: ' + str(len(pred_file['go_IDs'])))
-                curr_trial_preds = pred_file['Y_hat_test'][trial]
-                curr_trial_labels = pred_file['Y_test'][trial]
+                curr_trial_preds = pred_file['Y_hat_test']
+                curr_trial_labels = pred_file['Y_test']
                 print('Num test: ' + str(curr_trial_preds.shape[0]))
                 curr_macro, curr_micro, curr_acc, curr_f1 = evaluate_performance(curr_trial_labels, curr_trial_preds, curr_trial_preds > 0.5)
                 trial_macros.append(curr_macro)
@@ -106,48 +106,84 @@ def load_perfs(fnames):
                 trial_accs.append(curr_acc)
                 trial_f1s.append(curr_f1)
 
-        elif fname[-5:] == '.pckl': # now assuming pred file instead
-            pred_file = pickle.load(open(fname, "rb"))
-            num_trials = len(pred_file['trial_splits'])
-            trial_macros = []
-            trial_micros = []
-            trial_accs = []
-            trial_f1s = []
-            for trial in range(0, num_trials):
+            elif fname[-5:] == '.pckl': # now assuming pred file instead
+                pred_file = pickle.load(open(fname, "rb"))
+                trial_macros = []
+                trial_micros = []
+                trial_accs = []
+                trial_f1s = []
                 print('Number of go ids: ' + str(len(pred_file['GO_IDs'])))
-                curr_trial_test_inds = pred_file['trial_splits'][trial][1]
-                curr_trial_train_inds = pred_file['trial_splits'][trial][0]
-                curr_trial_preds = pred_file['trial_preds'][trial][curr_trial_test_inds]
-                curr_trial_labels = pred_file['true_labels'][curr_trial_test_inds]
+                curr_trial_preds = pred_file['preds']
+                curr_trial_labels = pred_file['true_labels']
                 curr_macro, curr_micro, curr_acc, curr_f1 = evaluate_performance(curr_trial_labels, curr_trial_preds, curr_trial_preds > 0.5)
-                print('Num train: ' + str(len(curr_trial_train_inds)))
-                print('Num test: ' + str(len(curr_trial_test_inds)))
+                print('Num test: ' + str(curr_trial_labels.shape[0]))
                 trial_macros.append(curr_macro)
                 trial_micros.append(curr_micro)
                 trial_accs.append(curr_acc)
                 trial_f1s.append(curr_f1)
-                
-        elif fname[-4:] == '.mat': # worry about this one later, this is for GeneMANIA
-            perfs = sio.loadmat(fname)
-            perfs = perfs['all_aups']
-            trial_macros = np.nanmean(perfs, axis=0)
-        elif fname[-4:] == '.txt':
-            trial_macros = []
-            trial_micros = []
-            trial_accs = []
-            trial_f1s = []
-            for line in open(fname, 'r'):
-                fields = line.split(' ') 
-                if len(fields) > 3 and is_number(fields[0]):
-                    trial_micros.append(float(fields[0]))
-                    trial_macros.append(float(fields[1]))
-                    trial_accs.append(float(fields[2]))
-                    trial_f1s.append(float(fields[3]))
+        else:
+            if '_scores.pckl' in fname: # now assuming BLAST pred files
+                pred_file = pickle.load(open(fname, 'rb'))
+                num_trials = len(pred_file['Y_hat_test_list'])
+                print('BLAST baseline')
+                trial_macros = []
+                trial_micros = []
+                trial_accs = []
+                trial_f1s = []
+                for trial in range(0, num_trials):
+                    print('Number of go ids: ' + str(len(pred_file['go_IDs'])))
+                    curr_trial_preds = pred_file['Y_hat_test_list'][trial]
+                    curr_trial_labels = pred_file['Y_test_list'][trial]
+                    print('Num test: ' + str(curr_trial_preds.shape[0]))
+                    curr_macro, curr_micro, curr_acc, curr_f1 = evaluate_performance(curr_trial_labels, curr_trial_preds, curr_trial_preds > 0.5)
+                    trial_macros.append(curr_macro)
+                    trial_micros.append(curr_micro)
+                    trial_accs.append(curr_acc)
+                    trial_f1s.append(curr_f1)
+
+            elif fname[-5:] == '.pckl': # now assuming pred file instead
+                pred_file = pickle.load(open(fname, "rb"))
+                trial_macros = []
+                trial_micros = []
+                trial_accs = []
+                trial_f1s = []
+                num_trials = len(pred_file['trial_splits'])
+                for trial in range(0, num_trials):
+                    print('Number of go ids: ' + str(len(pred_file['GO_IDs'])))
+                    curr_trial_test_inds = pred_file['trial_splits'][trial][1]
+                    curr_trial_train_inds = pred_file['trial_splits'][trial][0]
+                    curr_trial_preds = pred_file['trial_preds'][trial][curr_trial_test_inds]
+                    curr_trial_labels = pred_file['true_labels'][curr_trial_test_inds]
+                    curr_macro, curr_micro, curr_acc, curr_f1 = evaluate_performance(curr_trial_labels, curr_trial_preds, curr_trial_preds > 0.5)
+                    print('Num train: ' + str(len(curr_trial_train_inds)))
+                    print('Num test: ' + str(len(curr_trial_test_inds)))
+                    trial_macros.append(curr_macro)
+                    trial_micros.append(curr_micro)
+                    trial_accs.append(curr_acc)
+                    trial_f1s.append(curr_f1)
+
+                    
+            elif fname[-4:] == '.mat': # worry about this one later, this is for GeneMANIA
+                perfs = sio.loadmat(fname)
+                perfs = perfs['all_aups']
+                trial_macros = np.nanmean(perfs, axis=0)
+            elif fname[-4:] == '.txt':
+                trial_macros = []
+                trial_micros = []
+                trial_accs = []
+                trial_f1s = []
+                for line in open(fname, 'r'):
+                    fields = line.split(' ') 
+                    if len(fields) > 3 and is_number(fields[0]):
+                        trial_micros.append(float(fields[0]))
+                        trial_macros.append(float(fields[1]))
+                        trial_accs.append(float(fields[2]))
+                        trial_f1s.append(float(fields[3]))
                 print(trial_macros)
-            trial_macros = np.array(trial_macros)
-            trial_micros = np.array(trial_micros)
-            trial_accs = np.array(trial_accs)
-            trial_f1s = np.array(trial_f1s)
+                trial_macros = np.array(trial_macros)
+                trial_micros = np.array(trial_micros)
+                trial_accs = np.array(trial_accs)
+                trial_f1s = np.array(trial_f1s)
 
         macro = np.nanmean(trial_macros)
         macros.append(macro)
@@ -189,20 +225,33 @@ def plot_bars(x, y, stds, x_label, y_label, ax, start_pos):
     ax.set_ylabel(y_label)
 
 
-def plot_bars_grouped_by_metric(method_names, metric_lists, metric_stds, x_label, ax):
+def plot_bars_grouped_by_metric(method_names, metric_lists, metric_stds, x_label, ax, alpha_testing=False):
     metric_names = ['Macro AUPR', 'Micro AUPR', 'Acc', 'F1 score']
     #color_hexes = ['#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffa600']
     #color_hexes = ['#130fff', '#ff0073', '#ff8600', '#a6ff00']
-    color_hexes = ['#5d63a6', '#c97199', '#f1a07d', '#dfe092']
+    #color_hexes = ['#5d63a6', '#c97199', '#f1a07d', '#dfe092']
+    if not alpha_testing:
+        color_hexes = ['#003f5c', '#58508d','#bc5090','#ff6361','#ffa600', '#dfe092', '#a6ff00']
+        capsize = 5
+    else:
+        color_hexes = ['#00141d', '#072c39', '#193d46', '#004c6d', '#29617d', '#45778d', '#608d9e', '#7ca3b0', '#98bac2', '#b6d1d5', '#d4e8e9', '#f3ffff']
+        capsize = 2
+
+    print('Number of colors:')
+    print(len(color_hexes))
+    print('Number of methods:')
+    print(len(method_names))
     # switch the metrics and the method names
     print(np.array(metric_lists).shape)
     print(np.array(method_names).shape)
     print(np.array(metric_stds).shape)
     for i in range(0, len(method_names)):
         x_pos = i + np.arange(0, len(metric_lists))*(len(method_names)+1)
+        print(len(method_names))
+        print(np.array(metric_lists).shape)
         methods_curr_metric = np.array(metric_lists)[:,i].tolist()
         methods_curr_metric_stds = np.array(metric_stds)[:,i].tolist()
-        ax.bar(x_pos, methods_curr_metric, width=1, yerr=methods_curr_metric_stds, capsize=5, label=method_names[i], color=color_hexes[i])
+        ax.bar(x_pos, methods_curr_metric, width=1, yerr=methods_curr_metric_stds, capsize=capsize, label=method_names[i], color=color_hexes[i])
     ax.set_xticks(np.arange(0, len(metric_lists))*(len(method_names)+1) + len(method_names)/2)
     ax.set_xticklabels(metric_names)
     ax.set_yticks(np.arange(0, 1.05, 0.05))
@@ -228,7 +277,16 @@ def plot_bars_all_metrics(method_names, metric_lists, metric_stds, x_label, ax):
 if __name__ == '__main__':
     labels = sys.argv[1].split(',')
     title = sys.argv[2]
-    fnames = sys.argv[3:]
+    leave_one_species_out = False
+    alpha_testing = False
+    if sys.argv[3] == 'loso':
+        leave_one_species_out = True
+    elif sys.argv[3] == 'alpha_testing':
+        alpha_testing = True
+    print('leave one species out?' + str(leave_one_species_out))
+    print('Alpha testing?' + str(alpha_testing))
+    fnames = sys.argv[4:]
+    
     branch_fnames = {'MF': [], 'BP': [], 'CC': []}
     for fname in fnames:
         if 'molecular_function' in fname:
@@ -240,18 +298,29 @@ if __name__ == '__main__':
         else:
             print('One of the filenames doesn\'t have the strings \'molecular_function\', \'cellular_component\', or \'biological_process\' in it. All performance filenames must have one of these to be included in the plots.')
             print(fname)
-    assert len(branch_fnames['MF']) == len(branch_fnames['CC']) == len(branch_fnames['BP'])
+    try:
+        assert len(branch_fnames['MF']) == len(branch_fnames['CC']) == len(branch_fnames['BP'])
+        for branch in ['MF', 'BP','CC']:
+            print(branch)
+            print(len(branch_fnames[branch]))
+    except AssertionError:
+        print('Not all branches have same number of files associated')
+        for branch in ['MF', 'BP','CC']:
+            print(branch)
+            print(branch_fnames[branch])
+            print(len(branch_fnames[branch]))
+        exit()
     # I want to input number of files and have it know to put all files in one plot, with the number of subplots being with the number of files
     fig, axes = plt.subplots(1, 3, constrained_layout=True)
     #fig_2, axes_2 = plt.subplots(1, 3, constrained_layout=True)
 
     for i, branch in enumerate(['MF', 'BP', 'CC']):
-        macros, macro_stds, micros, micro_stds, accs, acc_stds, f1s, f1_stds = load_perfs(branch_fnames[branch])
+        macros, macro_stds, micros, micro_stds, accs, acc_stds, f1s, f1_stds = load_perfs(branch_fnames[branch], loso=leave_one_species_out)
         metric_lists = [macros, micros, accs, f1s]
         metric_stds = [macro_stds, micro_stds, acc_stds, f1_stds]
         print(np.array(metric_lists).shape)
         print(np.array(metric_stds).shape)
-        plot_bars_grouped_by_metric(labels, metric_lists, metric_stds, branch, axes[i])
+        plot_bars_grouped_by_metric(labels, metric_lists, metric_stds, branch, axes[i], alpha_testing=alpha_testing)
         #plot_bars_all_metrics(labels, metric_lists, metric_stds, branch, axes_2[i])
         '''
         plot_bars(labels, macros, macro_stds, branch, 'Macro AUPR', axes[i])
